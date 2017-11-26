150031eb8be541cdcbf25b2be1a5338b55b82529
qntm <qntm@users.noreply.github.com>
2017-11-26 17:58:29 +0000

    Split out orientations to somewhere common

--- src/js/hatetris.js
+++ src/js/hatetris.js
@@ -13,54 +13,14 @@ import '../html/hatetris.html'
 
 import './statcounter'
 import build from './build'
-import oriGen from './ori-gen'
 import getGetWorstPiece from './get-get-worst-piece'
+import orientations from './orientations'
 
 // Fixed attributes of all of Tetris
-
-// Note that the order here is significant,
-// the least convenient piece is placed first.
-const pieces = [[
-  '....',
-  '..##',
-  '.##.',
-  '....'
-], [
-  '....',
-  '.##.',
-  '..##',
-  '....'
-], [
-  '....',
-  '.##.',
-  '.##.',
-  '....'
-], [
-  '....',
-  '####',
-  '....',
-  '....'
-], [
-  '....',
-  '.###',
-  '.#..',
-  '....'
-], [
-  '....',
-  '.##.',
-  '.#..',
-  '.#..'
-], [
-  '....',
-  '.###',
-  '..#.',
-  '....'
-]]
-
-const orientations = oriGen(pieces)
 const bar = 4
 const wellDepth = 20 // min = bar
 const wellWidth = 10 // min = 4
+
 const getWorstPiece = getGetWorstPiece(orientations, bar, wellDepth, wellWidth)
 const replayTimeout = 50 // milliseconds per frame
 
