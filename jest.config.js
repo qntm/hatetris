63a5b941283bd50d2fea527f1701c60f0609ec20
qntm <qntm@users.noreply.github.com>
2020-09-27 21:17:43 +0100

    Enforce

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,13 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.css$': '<rootDir>/__mocks__/styleMock.js'
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
   }
 }
