ff458259dc59afa4c7bd8ac52fa759803ce6abb6
qntm <qntm@users.noreply.github.com>
2021-05-08 20:52:14 +0100

    And finally

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,14 @@ import * as ReactDOM from 'react-dom'
 
 import './index.css'
 import Game from './components/Game/Game'
-import { Hatetris0 } from './enemy-ais/hatetris-ai'
+import { HatetrisAi } from './enemy-ais/hatetris-ai'
 import hatetrisRotationSystem from './rotation-systems/hatetris-rotation-system'
 
 ReactDOM.render(
   (
     <Game
       bar={4}
-      EnemyAi={Hatetris0}
+      EnemyAi={HatetrisAi}
       replayTimeout={50}
       rotationSystem={hatetrisRotationSystem}
       wellDepth={20}
