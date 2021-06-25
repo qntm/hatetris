dddf57df13b9cf920be0b6760f62e55c1073cb10
qntm <qntm@users.noreply.github.com>
2021-06-25 18:15:58 +0100

    Introducing Burgiel (1997) (#80)

---
+++ src/enemy-ais/burgiel.spec.tsx
@@ -0,0 +1,56 @@
+/* eslint-env jest */
+
+'use strict'
+
+import type { CoreState } from '../components/Game/Game'
+import { burgAi } from './burgiel'
+
+// Note: well bits are flipped compared to what you would see on the screen.
+// Least significant bit is rendered on the *left* on web, but appears to the
+// *right* of each binary numeric literal
+
+describe('burgAi', () => {
+  const getNextCoreStates = (): CoreState[] => []
+
+  it('generates an S at first', () => {
+    expect(burgAi({
+      score: 0,
+      well: [
+        0b000000,
+        0b000000,
+        0b000000,
+        0b000000,
+        0b000000,
+        0b000000
+      ]
+    }, undefined, getNextCoreStates)).toEqual(['S', 'Z'])
+  })
+
+  it('generates a Z next', () => {
+    expect(burgAi({
+      score: 0,
+      well: [
+        0b000000,
+        0b000000,
+        0b000000,
+        0b000001,
+        0b000011,
+        0b000010
+      ]
+    }, 'Z', getNextCoreStates)).toEqual(['Z', 'S'])
+  })
+
+  it('generates an S after that', () => {
+    expect(burgAi({
+      score: 0,
+      well: [
+        0b000000,
+        0b000000,
+        0b000000,
+        0b001001,
+        0b001111,
+        0b000110
+      ]
+    }, 'S', getNextCoreStates)).toEqual(['S', 'Z'])
+  })
+})
