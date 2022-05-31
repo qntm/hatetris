1792bc7a8a0f5e6afe4176e9673b289d58630e3e
qntm <qntm@users.noreply.github.com>
2022-05-31 01:38:25 +0100

    Improved cycle detection (#146)
    * Why won't this pass Cypress?
    
    * Upgrade Cypress
    
    * Add another record!

--- src/enemy-ais/hatetris-ai.ts
+++ src/enemy-ais/hatetris-ai.ts
@@ -2,12 +2,8 @@
 
 import type { CoreState, EnemyAi } from '../components/Game/Game.jsx'
 
-type Seen = {
-  well: number[],
-  pieceIds: string[]
-}
-
-type HatetrisAiState = Array<Seen>
+// Set containing stringified previously-seen wells
+type HatetrisAiState = Set<String>
 
 // S = worst for the player, T = best
 const worstPieces = 'SZOILJT'.split('')
@@ -25,44 +21,50 @@ export const hatetrisAi: EnemyAi = (
   ) => CoreState[]
 ): [string, HatetrisAiState] => {
   if (currentAiState === undefined) {
-    currentAiState = []
+    currentAiState = new Set()
   }
 
-  // Has this well been seen before? -1 => no
-  const prevIndex = currentAiState
-    .findIndex(({ well }) =>
-      well.every((row: number, y) => row === currentCoreState.well[y])
-    )
+  const nextAiState = new Set([
+    ...currentAiState,
+    JSON.stringify(currentCoreState.well)
+  ])
 
   const evaluations = worstPieces
-    .map((pieceId, pieceRanking) => ({
-      pieceId,
+    .map((pieceId, pieceRanking) => {
+      const nextCoreStates = getNextCoreStates(currentCoreState, pieceId)
 
-      // Has this piece been generated before for this well? 0 => no
-      alreadyGenerated: prevIndex === -1
-        ? 0
-        : currentAiState[prevIndex].pieceIds.includes(pieceId)
-          ? 1
-          : 0,
+      // Yes, test against `nextAiState` because what if the current width
+      // is 4 and the candidate piece is an I which, if placed, leads back into
+      // the current well?
+      const leadsIntoCycle = Number(nextCoreStates.some(nextCoreState =>
+        nextAiState.has(JSON.stringify(nextCoreState.well))
+      ))
 
-      highestPeak: Math.max(
-        ...getNextCoreStates(currentCoreState, pieceId)
-          .map(nextCoreState => {
-            const rating = nextCoreState.well.findIndex((row: number) => row !== 0)
+      let highestPeak = -Infinity
+      nextCoreStates.forEach(nextCoreState => {
+        let rating = nextCoreState.well.findIndex((row: number) => row !== 0)
+        if (rating === -1) {
+          // Well is completely empty after placing this piece in this location
+          rating = nextCoreState.well.length
+        }
 
-            return rating === -1
-              // Well is completely empty after placing this piece in this location
-              ? nextCoreState.well.length
-              : rating
-          })
-      ),
-      pieceRanking
-    }))
+        if (rating > highestPeak) {
+          highestPeak = rating
+        }
+      })
+
+      return {
+        pieceId,
+        leadsIntoCycle,
+        highestPeak,
+        pieceRanking
+      }
+    })
 
   evaluations.sort((a, b) =>
-    // Always prefer a piece which has never been generated before for this well over a piece
-    // which has already been generated for this well.
-    (a.alreadyGenerated - b.alreadyGenerated) ||
+    // Always prefer a piece which does NOT potentially lead into a previously
+    // seen well.
+    (a.leadsIntoCycle - b.leadsIntoCycle) ||
 
     // Otherwise, whichever piece gives the highest peak (lower number = higher peak)
     (a.highestPeak - b.highestPeak) ||
@@ -75,28 +77,6 @@ export const hatetrisAi: EnemyAi = (
 
   return [
     pieceId,
-    prevIndex === -1
-      ? [
-          ...currentAiState,
-          {
-            well: currentCoreState.well,
-            pieceIds: [pieceId]
-          }
-        ]
-      : currentAiState[prevIndex].pieceIds.includes(pieceId)
-        // No duplicates. Approximately 0% probability of this ever happening with this
-        // AI, but as a general technique it's worth having this on the books
-        ? currentAiState
-        : [
-            ...currentAiState.slice(0, prevIndex),
-            {
-              ...currentAiState[prevIndex],
-              pieceIds: [
-                ...currentAiState[prevIndex].pieceIds,
-                pieceId
-              ]
-            },
-            ...currentAiState.slice(prevIndex + 1)
-          ]
+    nextAiState
   ]
 }
