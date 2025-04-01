acd5d02ea010500a3c8fd689a60ddea705a522af
qntm <qntm@users.noreply.github.com>
2025-04-01 21:45:21 +0100

    React 19

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,12 @@ import './components/Game/Game.css'
 import './components/Well/Well.css'
 
 import * as React from 'react'
-import { createRoot } from 'react-dom/client'
+import * as ReactDOM from 'react-dom/client'
 
 import Game from './components/Game/Game.jsx'
 import hatetrisRotationSystem from './rotation-systems/hatetris-rotation-system.js'
 
-const root = createRoot(document.querySelector('.index__root'))
+const root = ReactDOM.createRoot(document.querySelector('.index__root'))
 root.render(
   <Game
     bar={4}
