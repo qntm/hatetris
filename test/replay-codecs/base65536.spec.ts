47512dccd7201d154e38efcb39acdd5fe9da3703
qntm <qntm@users.noreply.github.com>
2025-12-22 00:37:56 +0000

    Update base65536.spec.ts

--- test/replay-codecs/base65536.spec.ts
+++ test/replay-codecs/base65536.spec.ts
@@ -1,17 +1,18 @@
-import * as assert from 'node:assert'
+import assert from 'node:assert/strict'
+
 import { describe, it } from 'mocha'
 import * as base65536 from '../../src/replay-codecs/base65536.js'
 
 describe('base65536', () => {
   it('works', () => {
-    assert.strictEqual(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D']), '𤺤')
-    assert.deepStrictEqual(base65536.decode('𤺤'), ['D', 'D', 'D', 'R', 'U', 'D'])
+    assert.equal(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D']), '𤺤')
+    assert.deepEqual(base65536.decode('𤺤'), ['D', 'D', 'D', 'R', 'U', 'D'])
   })
 
   it('adds an extra run to shore up the octet count if need be', () => {
-    assert.strictEqual(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D', 'U', 'U']), '𤺤ᗐ')
-    assert.strictEqual(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D', 'U', 'U', 'L']), '𤺤ᗐ')
-    assert.deepStrictEqual(base65536.decode('𤺤ᗐ'),
+    assert.equal(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D', 'U', 'U']), '𤺤ᗐ')
+    assert.equal(base65536.encode(['D', 'D', 'D', 'R', 'U', 'D', 'U', 'U', 'L']), '𤺤ᗐ')
+    assert.deepEqual(base65536.decode('𤺤ᗐ'),
       ['D', 'D', 'D', 'R', 'U', 'D', 'U', 'U', 'L'])
   })
 })
