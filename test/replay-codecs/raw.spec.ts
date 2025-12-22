2d15f7470e8aefb18fc021d3ec99b25a310d1427
qntm <qntm@users.noreply.github.com>
2025-12-22 00:38:04 +0000

    Update raw.spec.ts

--- test/replay-codecs/raw.spec.ts
+++ test/replay-codecs/raw.spec.ts
@@ -1,10 +1,11 @@
-import * as assert from 'node:assert'
+import assert from 'node:assert/strict'
+
 import { describe, it } from 'mocha'
 import * as raw from '../../src/replay-codecs/raw.js'
 
 describe('raw', () => {
   it('raw', () => {
-    assert.strictEqual(raw.encode(['D', 'U', 'L', 'L', 'A', 'R', 'D']), 'DULLARD')
-    assert.deepStrictEqual(raw.decode('DULLARD'), ['D', 'U', 'L', 'L', 'A', 'R', 'D'])
+    assert.equal(raw.encode(['D', 'U', 'L', 'L', 'A', 'R', 'D']), 'DULLARD')
+    assert.deepEqual(raw.decode('DULLARD'), ['D', 'U', 'L', 'L', 'A', 'R', 'D'])
   })
 })
